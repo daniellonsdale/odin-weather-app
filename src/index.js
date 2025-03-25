25d1262a3562dcab9506df009b76c6b8f7ace4ff
daniellonsdale <danielllonsdale@gmail.com>
2025-03-26 00:56:41 +0900

    Return icon as well

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ async function callWeatherAPI(location, unit){
     try {
         let initCall = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=S7RTNY7YMKM6ZUE7HJ9DZK3B9&unitGroup=${unit}`, {mode: 'cors'});
         let callJson = await initCall.json();
-        let importantInfo = {'conditions': callJson.currentConditions.conditions, 'description': callJson.description, 'temp': callJson.currentConditions.temp, 'feelslike': callJson.currentConditions.feelslike, 'humidity': callJson.currentConditions.humidity, 'uvindex': callJson.currentConditions.uvindex};
+        let importantInfo = {'conditions': callJson.currentConditions.conditions, 'description': callJson.description, 'temp': callJson.currentConditions.temp, 'feelslike': callJson.currentConditions.feelslike, 'humidity': callJson.currentConditions.humidity, 'uvindex': callJson.currentConditions.uvindex, 'icon': callJson.currentConditions.icon};
         console.log(importantInfo);
     } catch (error) {
         console.log(error);
     }
 }
-
-callWeatherAPI();
