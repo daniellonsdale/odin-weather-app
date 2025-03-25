9be2a6747934d2d89d9cc04b11cf4e22e87f1033
daniellonsdale <danielllonsdale@gmail.com>
2025-03-26 00:54:21 +0900

    Add unit functionality

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import "./styles.css";
 
 async function callWeatherAPI(location, unit){
     location = 'tokyo';
+    unit = 'metric';
     try {
-        let initCall = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=S7RTNY7YMKM6ZUE7HJ9DZK3B9`, {mode: 'cors'});
+        let initCall = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=S7RTNY7YMKM6ZUE7HJ9DZK3B9&unitGroup=${unit}`, {mode: 'cors'});
         let callJson = await initCall.json();
         let importantInfo = {'conditions': callJson.currentConditions.conditions, 'description': callJson.description, 'temp': callJson.currentConditions.temp, 'feelslike': callJson.currentConditions.feelslike, 'humidity': callJson.currentConditions.humidity, 'uvindex': callJson.currentConditions.uvindex};
         console.log(importantInfo);
@@ -11,3 +12,5 @@ async function callWeatherAPI(location, unit){
         console.log(error);
     }
 }
+
+callWeatherAPI();
