652f1a03538a0b660ea7e201a565739dfef2da96
daniellonsdale <danielllonsdale@gmail.com>
2025-03-26 00:51:54 +0900

    Create init callWeatherAPI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.css";
+
+async function callWeatherAPI(location, unit){
+    location = 'tokyo';
+    try {
+        let initCall = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=S7RTNY7YMKM6ZUE7HJ9DZK3B9`, {mode: 'cors'});
+        let callJson = await initCall.json();
+        let importantInfo = {'conditions': callJson.currentConditions.conditions, 'description': callJson.description, 'temp': callJson.currentConditions.temp, 'feelslike': callJson.currentConditions.feelslike, 'humidity': callJson.currentConditions.humidity, 'uvindex': callJson.currentConditions.uvindex};
+        console.log(importantInfo);
+    } catch (error) {
+        console.log(error);
+    }
+}
