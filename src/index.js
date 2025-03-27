98fd82f2cc1474a425844e37bbe0c112e2c5f86c
daniellonsdale <danielllonsdale@gmail.com>
2025-03-28 00:40:21 +0900

    Create updateDOM function

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const currentWeatherWind = document.querySelector('.wind');
 
 let metric = true;
 let currentLocation = 'Tokyo';
-callWeatherAPI(currentLocation, 'metric');
+callWeatherAPI(currentLocation, 'metric').then(updateDOM);
 
 async function callWeatherAPI(location, unit){
     location = 'tokyo';
@@ -23,9 +23,22 @@ async function callWeatherAPI(location, unit){
     try {
         let initCall = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=S7RTNY7YMKM6ZUE7HJ9DZK3B9&unitGroup=${unit}`, {mode: 'cors'});
         let callJson = await initCall.json();
-        let importantInfo = {'conditions': callJson.currentConditions.conditions, 'description': callJson.description, 'temp': callJson.currentConditions.temp, 'feelslike': callJson.currentConditions.feelslike, 'humidity': callJson.currentConditions.humidity, 'uvindex': callJson.currentConditions.uvindex, 'icon': callJson.currentConditions.icon};
-        console.log(importantInfo);
+        let importantInfo = {'conditions': callJson.currentConditions.conditions, 'description': callJson.description, 'temp': callJson.currentConditions.temp, 'feelslike': callJson.currentConditions.feelslike, 'humidity': callJson.currentConditions.humidity, 'uvindex': callJson.currentConditions.uvindex, 'icon': callJson.currentConditions.icon + '.svg', 'address': callJson.address, 'unit': unit, 'windspeed': callJson.currentConditions.windspeed};
+        return importantInfo;
     } catch (error) {
         console.log(error);
     }
+}
+
+function updateDOM(newWeatherObject){
+    locationNameDiv.textContent = newWeatherObject.address;
+    currentWeatherNumber.textContent = newWeatherObject.temp;
+    fcDisplay.textContent  = newWeatherObject.unit;
+    currentWeatherIcon.src = `icons/${newWeatherObject.icon}`;
+    currentWeatherText.textContent = newWeatherObject.conditions;
+    currentWeatherDescription.textContent = newWeatherObject.description;
+    currentWeatherFeelsLike.textContent = newWeatherObject.feelslike;
+    currentWeatherHumidity.textContent = newWeatherObject.humidity;
+    currentWeatherUVIndex.textContent = newWeatherObject.uvindex;
+    currentWeatherWind.textContent = newWeatherObject.windspeed;
 }
